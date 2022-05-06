cdca945cc398eda231a0ac444cfdba1de297c503
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-05-06 15:26:29 +0200

    normal form input relation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 const Form = () => {
+  const [list, setList] = useState({
+    question: "",
+    answer: ""
+  })
+
+  const handleInputChange = (event) => {
+    //console.log(event.target.value);
+    setList({
+      ...list,
+      [event.target.name] : event.target.value
+    })
+  }
+
+
+
   return (
     <div>
-      <p>
-      Hola soy un formulario
-      </p>
+      <form className="row">
+        <div className="col-md-3">
+          <input
+            className="form-control"
+            type="text"
+            name="question"
+            onChange={handleInputChange}
+            placeholder="Enter your question"
+          />
+        </div>
+        <div className="col-md-3">
+          <input
+            className="form-control"
+            type="text"
+            name="answer"
+            onChange={handleInputChange}
+            placeholder="The answer goes here"
+          />
+        </div>
+        <button className="btn btn-primary submit-button" type="submit">Add question</button>
+      </form>
     </div>
   )
 }
