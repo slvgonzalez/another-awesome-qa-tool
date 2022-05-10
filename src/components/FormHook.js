34e94b538c9a73e57ff391ac131ec4bc21c4d9c0
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-05-10 05:38:00 +0200

    responsive form

--- src/components/FormHook.js
+++ src/components/FormHook.js
@@ -23,32 +23,33 @@ const FormHook = (props) => {
       <div id="anim">
         <span class="tooltip" data-tooltip="You can find created questions and answers here.">?</span>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)} className="form-group">
-        <label htmlFor="name">Question:</label>
-        <input
-            className="form-control"
-            id="question"
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label htmlFor="name">Question:</label>
+          <input
+              className="form-control"
+              id="question"
+              type="text"
+              placeholder="Type your question here"
+              {...register('question', { required: true, minLength: 10 })}
+          />
+
+          { errors.question && errors.question.type === "required" && <span className="text-danger text-small d-block mb-2">A question is required.</span> }
+          {errors.question && errors.question.type === "minLength" && <span className="text-danger text-small d-block mb-2">The question must have at least 10 characters.</span>}
+
+          <label htmlFor="name">Answer:</label>
+          <input
+            className="form-control my-2"
+            id="answer"
             type="text"
-            placeholder="Type your question here"
-            {...register('question', { required: true, minLength: 10 })}
-        />
+            rows="4"
+            placeholder="Type your answer here"
+            {...register('answer', { required: true, minLength: 10 })}
+          />
 
-        { errors.question && errors.question.type === "required" && <span className="text-danger text-small d-block mb-2">A question is required.</span> }
-        {errors.question && errors.question.type === "minLength" && <span className="text-danger text-small d-block mb-2">The question must have at least 10 characters.</span>}
-
-        <label htmlFor="name">Answer:</label>
-        <input
-          className="form-control my-2"
-          id="answer"
-          type="text"
-          rows="4"
-          placeholder="Type your answer here"
-          {...register('answer', { required: true, minLength: 10 })}
-        />
-
-        {errors.answer && errors.answer.type === "required" && <span className="text-danger text-small d-block mb-2">An answer is required</span>}
-        {errors.answer && errors.answer.type === "minLength" && <span className="text-danger text-small d-block mb-2">The answer must be elavorated (min 10 characters).</span>}
-
+          {errors.answer && errors.answer.type === "required" && <span className="text-danger text-small d-block mb-2">An answer is required</span>}
+          {errors.answer && errors.answer.type === "minLength" && <span className="text-danger text-small d-block mb-2">The answer must be elavorated (min 10 characters).</span>}
+        </div>
         <button className="btn btn-add">Add question</button>
       </form>
     </>
